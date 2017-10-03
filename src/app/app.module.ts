19250a0f06428e7cb2624b2a9471452323b0e9d8
Honderdos, Serge (ITCDCC) - KLM <Serge.Honderdos@klm.com>
2017-10-03 16:59:06 +0200

    created login and register screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from "@angular/router";
+import {MatButtonModule} from '@angular/material';
+import {MatCardModule} from '@angular/material';
+import {MatInputModule} from '@angular/material';
 
-import { AppComponent } from './app.component';
+
+// Angular Material Modules
+import {MdToolbarModule} from '@angular/material';
+
+import {AppComponent} from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+    {path:'', component:DashboardComponent},
+    {path:'login', component:LoginComponent},
+    {path:'register', component:RegisterComponent},
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        LoginComponent,
+        RegisterComponent,
+        NavbarComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MdToolbarModule,
+        MatButtonModule,
+        MatCardModule,
+        MatInputModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
